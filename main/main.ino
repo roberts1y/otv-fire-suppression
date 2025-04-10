478fbdcfdeb533791b1d5d50aa2d35bca29d2b6e
Karsten Greninger <152125546+KarKarBinks123@users.noreply.github.com>
2025-04-10 18:49:43 -0400

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -4,9 +4,12 @@
 #include "MotorControl.h"
 #include <math.h>
 
+const int TXPin = 0;
+const int RXPin = 1;
+
 void setup() {
   // Initialize ENES100 system
-  Enes100.begin("Fire Suppression", FIRE, 11, 1120, 0, 7);
+  Enes100.begin("Fire Suppression", FIRE, 11, 1120, TXPin, RXPin);
 
   // Set up motor control pins
   setupMotors();
