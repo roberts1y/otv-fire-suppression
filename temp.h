a1e2bb8a8fd30ac6427c1383cfdb0720302f3fb9
Dejones-terp <dejones@terpmail.umd.edu>
2025-04-10 19:21:04 -0400

    Update temp.h

--- temp.h
+++ temp.h
@@ -1,18 +1,5 @@
-#include "DHT.h"
+#include temp.h
 
-// Define the pin for the DHT22 data line and the LED
-#define DHTPIN1 5
-#define DHTPIN2 6
-#define DHTPIN3 9
-#define DHTPIN4 10
-
-// Define the sensor type (DHT22)
-#define DHTTYPE DHT22
-
-DHT dht1(DHTPIN1, DHTTYPE);
-DHT dht2(DHTPIN2, DHTTYPE);
-DHT dht3(DHTPIN3, DHTTYPE);
-DHT dht4(DHTPIN4, DHTTYPE);
 
 void setup(){
   
