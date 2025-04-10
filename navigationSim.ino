c1d2d804c33ff47c375770e6e0cd5bc2786a14f4
Karsten Greninger <152125546+KarKarBinks123@users.noreply.github.com>
2025-04-10 18:59:47 -0400

    Update navigationSim.ino

--- navigationSim.ino
+++ navigationSim.ino
@@ -1,7 +1,6 @@
 #include <math.h>
 #include "Arduino.h"
 #include "Enes100.h"
-#include "Tank.h"
 
 const float positionTolerance = 0.1;  // meters
 const float obstacleTolerance = 5; //cm
@@ -10,18 +9,6 @@ const float angleTolerance = 0.1;      // radians
 const int trigPin3 = 4;
 const int echoPin3 = 5;
 
-
-void setup() {
-    Enes100.begin("Simulator", FIRE, 3, 8, 9);
-    Tank.begin();
-    
-    Enes100.println("Ready to navigate with obstacle avoidance.");
-}
-
-void loop() {
-    goToPoint(3, 0.4);  // Move to this point and face 0 radians
-    while (true);  // Stop the loop after reaching
-}
 
 // Move to (targetX, targetY) and face 0 radians
 void goToPoint(float targetX, float targetY) {
@@ -36,8 +23,7 @@ void goToPoint(float targetX, float targetY) {
 
         // If the robot has reached the target position
         if (distance < positionTolerance) {
-            Tank.setLeftMotorPWM(0);
-            Tank.setRightMotorPWM(0);
+            moveForward(0);
             Enes100.println("Arrived at destination.");
             break;
         }
@@ -50,21 +36,16 @@ void goToPoint(float targetX, float targetY) {
         if (abs(dTheta) > angleTolerance) {
             // Robot needs to turn
             if (dTheta > 0) {
-                Tank.setLeftMotorPWM(-100);  // Turn left
-                Tank.setRightMotorPWM(100);  // Turn right
+                spinLeft(90);
             } else {
-                Tank.setLeftMotorPWM(100);  // Turn right
-                Tank.setRightMotorPWM(-100);  // Turn left
+                spinRight(90);
             }
         } else {
             // Move forward after reaching the correct angle
-            Tank.setLeftMotorPWM(255);  // Full speed forward
-            Tank.setRightMotorPWM(255);
-            moved = true;  // Full speed forward
+            moveForward(90);
         }
 
         if(getDistance(trigPin3, echoPin3, 1) < obstacleTolerance){
-
             avoidObstacle();        
         }
         
@@ -76,17 +57,14 @@ void avoidObstacle() {
     Enes100.println("Avoiding obstacle...");
 
     // Stop
-    Tank.setLeftMotorPWM(0);
-    Tank.setRightMotorPWM(0);
+    moveForward(0);
     delay(500);  // Wait for half a second to stop
 
     // Turn left for 90 degrees (fixed turn)
-    Tank.setLeftMotorPWM(-255);  // Max speed turn left
-    Tank.setRightMotorPWM(255);  // Max speed turn right
+    spinLeft(90);
     delay(670);  // Turn for 1 second (adjust based on needs)
 
     // Move forward again after avoiding the obstacle
-    Tank.setLeftMotorPWM(255);  // Max speed forward
-    Tank.setRightMotorPWM(255);  // Max speed forward
+    moveForward(90);
     delay(500);  // Drive for half a second
 }
