3221939b3c8d8f46126a110b1ec4e402b6384c48
Dejones-terp <dejones@terpmail.umd.edu>
2025-04-15 18:51:00 -0400

    Update temp.h

--- main/temp.h
+++ main/temp.h
@@ -1,6 +1,12 @@
 #ifndef TEMP_H
 #define TEMP_H
 
+  // Variable to store the previous temperature readings
+float prevTemperature1 = NAN;  // NAN indicates that we haven't taken a reading yet
+float prevTemperature2 = NAN;  // NAN indicates that we haven't taken a reading yet
+float prevTemperature3 = NAN;  // NAN indicates that we haven't taken a reading yet
+float prevTemperature4 = NAN;  // NAN indicates that we haven't taken a reading yet
+
 void printTemp1();
 void printTemp2();
 void printTemp3();
