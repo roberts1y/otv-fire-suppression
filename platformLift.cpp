e6f10c5888ee9b37469753507747a898edd5a15b
Dejones-terp <dejones@terpmail.umd.edu>
2025-04-17 19:07:12 -0400

    Update platformLift.cpp

--- platformLift.cpp
+++ platformLift.cpp
@@ -1,15 +1,31 @@
 #include <Servo.h>
 
 Servo servo1;
-void setup(){
-// put your setup code here, to run once:
-servo1.attach(9);
-}
 
-void loop(){
-//put your main code here, to run repeatedly:
+void lifting(){
+servo1.attach(9);
+  
 servo1.writeMicroseconds(200);
 delay(3000);
 servo1.writeMicroseconds(500);
 delay(3000);
+}
+
+void dropping(){
+servo1.attach(9);
+  
+servo1.writeMicroseconds(200);
+delay(3000);
+servo1.writeMicroseconds(500);
+delay(3000);
+
+void setup(){
+// put your setup code here, to run once:
+
+}
+
+void loop(){
+//put your main code here, to run repeatedly:
+dropping();
+lifting();
 }
