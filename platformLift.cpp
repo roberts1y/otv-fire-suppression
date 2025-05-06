3b0b15665456af9ea891270540fd03f274e464cd
Dejones-terp <dejones@terpmail.umd.edu>
2025-05-06 18:17:39 -0400

    Update platformLift.cpp

--- platformLift.cpp
+++ platformLift.cpp
@@ -19,14 +19,4 @@ servo1.writeMicroseconds(200);
 delay(3000);
 servo1.writeMicroseconds(500);
 delay(3000);
-
-void setup(){
-// put your setup code here, to run once:
-
-}
-
-void loop(){
-//put your main code here, to run repeatedly:
-dropping();
-lifting();
 }
