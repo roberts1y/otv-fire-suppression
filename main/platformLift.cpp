a984add93791e1321c8e2ff4df08435a83d8fb9a
jtao12 <jtao12@terpmail.umd.edu>
2025-05-07 20:28:28 -0400

    Update platformLift.cpp

--- main/platformLift.cpp
+++ main/platformLift.cpp
@@ -1,33 +1,28 @@
 #include <Servo.h>
 #include <Arduino.h>
 
-Servo servo1;
-Servo servo2;
+Servo servo;
 
-#define servoPin1 41
-#define servoPin2 39
+
+#define servoPin141
+
+
+void setup() {
+  servo.attach(servoPin);
+
+}
 
 void lifting(){
-servo1.attach(servoPin1);
-servo2.attach(servoPin2);
-  
-servo1.writeMicroseconds(200);
-servo2.writeMicroseconds(200);
-delay(3000);
-servo1.writeMicroseconds(500);
-servo2.writeMicroseconds(500);
-delay(3000);
+
+servo.writeMicroseconds(544);
+delay(10000);
+
 }
 
 //make sure you set this to spin in the opposite direction
 void dropping(){
-servo1.attach(servoPin1);
-servo2.attach(servoPin2);
 
-servo1.writeMicroseconds(200);
-servo2.writeMicroseconds(200);
-delay(3000);
-servo2.writeMicroseconds(500);
-servo1.writeMicroseconds(500);
-delay(3000);
+servo.writeMicroseconds(2400);
+delay(10000);
+
 }
